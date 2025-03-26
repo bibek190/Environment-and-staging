48cca23a4fbd3301c1a67c6fdc7ba8f2165ae165
bibek190 <bibek9818917036@gmail.com>
2025-03-26 11:20:51 +1100

    add shadow to the plane, shadows with the lights

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,14 @@
 import { useFrame } from "@react-three/fiber";
-import {
-  BakeShadows,
-  OrbitControls,
-  SoftShadows,
-  useHelper,
-} from "@react-three/drei";
+import { BakeShadows, OrbitControls, useHelper } from "@react-three/drei";
 import { useRef } from "react";
 import { Perf } from "r3f-perf";
 import * as THREE from "three";
 
 export default function Experience() {
   const cube = useRef();
+
   const directionalLightRef = useRef();
-  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1);
+  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 2);
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
@@ -20,42 +16,37 @@ export default function Experience() {
 
   return (
     <>
-      <color args={["ivory"]} attach="background" />
-
+      <BakeShadows />
       <Perf position="top-left" />
-
-      <OrbitControls />
-
+      <color args={["ivory"]} attach="background" />
+      <OrbitControls makeDefault />
       <directionalLight
-        ref={directionalLightRef}
         castShadow
+        ref={directionalLightRef}
+        position={[1, 2, 3]}
+        intensity={4.5}
         shadow-mapSize={[1024, 1024]}
         shadow-camera-near={1}
         shadow-camera-far={10}
-        shadow-camera-top={5}
-        shadow-camera-right={5}
-        shadow-camera-bottom={-5}
-        shadow-camera-left={-5}
-        position={[1, 2, 3]}
-        intensity={4.5}
+        shadow-camera-top={2}
+        shadow-camera-right={2}
+        shadow-camera-bottom={-2}
+        shadow-camera-left={-2}
       />
       <ambientLight intensity={1.5} />
-
-      <mesh castShadow position-x={-2}>
+      <mesh position-x={-2} castShadow>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
-
-      <mesh castShadow ref={cube} position-x={2} scale={1.5}>
+      <mesh ref={cube} position-x={2} scale={1.5} castShadow>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
-
       <mesh
-        receiveShadow
         position-y={-1}
         rotation-x={-Math.PI * 0.5}
         scale={10}
+        receiveShadow
       >
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
-import * as THREE from "three";
+import { BackSide } from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
@@ -10,7 +10,7 @@ root.render(
   <Canvas
     shadows
     camera={{
-      fov: 50,
+      fov: 45,
       near: 0.1,
       far: 200,
       position: [-4, 3, 6],
